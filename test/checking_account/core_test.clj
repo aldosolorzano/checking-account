1c17ee182df6fa8e2551161ac9cbb0c7f99d9cc6
aldo <aldosolorzanop@gmail.com>
2019-03-13 09:42:56 -0600

    core tests

--- test/checking_account/core_test.clj
+++ test/checking_account/core_test.clj
@@ -1,31 +1,34 @@
 (ns checking-account.core-test
   (:require [clojure.test :refer :all]
-            [checking-account.core :refer :all]))
+            [checking-account.core :refer :all]
+            [checking-account.db :as db]
+            [checking-account.date_helpers :as d]))
 
 (def fresh-account {:id 102 :tx-ids (atom [])})
-(def transaction-params {:description "Deposit" :amount 180 :date "17/10" :type "withdrawal"})
+(def account-id 100)
 
-(deftest Checking-account
-  (testing "Compute Balance"
-    (is (= (compute-balance @transactions-db []) 0))
-    (is (= (compute-balance @transactions-db (deref (get (get-account-by-id @accounts-db 100) :tx-ids))) 771.4300000000001)))
-
+(deftest Get-balance
   (testing "Get Balance"
-    (is (= (get-balance (deref (get (get-account-by-id @accounts-db 100) :tx-ids))) "771.43"))
-    (is (= (get-balance []) "0.00"))
-    (is (= (get-balance (deref (get (get-account-by-id @accounts-db 101) :tx-ids))) "0.00")))
+    (is (= (get-balance @db/transactions (@db/accounts account-id)) "-38.57"))
+    (is (= (get-balance @db/transactions {:id 102 :tx-ids (atom [0 1 2 3])}) "771.43"))
+    (is (= (get-balance @db/transactions {:id 102 :tx-ids (atom [0 1 2 3 4])}) "-28.57"))
+    (is (= (get-balance @db/transactions fresh-account) "0.00"))))
 
-  (testing "Create account"
-    (let [account-id (create-account @accounts-db)]
-      (is (= (get fresh-account :id) account-id))
-      (is (empty? (deref (get-in @accounts-db [account-id :tx-ids]))))))
+(def negative-body [{:principal "28.57" :start "18/10" :end "22/10"} {:principal "38.57" :start "22/10"}])
 
-  (testing "Create transaction"
-    (let [tx (create-transaction @transactions-db (get fresh-account :id) transaction-params)]
-      (println tx)
-      (is (= (get tx :id) 4))
-      (is (= (get fresh-account :id) (get tx :account)))
-      (is (= (get transaction-params :description) (get tx :description)))
-      (is (= (keyword(get transaction-params :type)) (get tx :type)))
-      (is (= (get transaction-params :amount) (get tx :amount)))
-      )))
+(deftest Negative-periods
+  (testing "Build vector of maps with negative periods"
+      (is (= (negative-periods @db/transactions (@db/accounts account-id)) negative-body))
+      (is (empty? (negative-periods @db/transactions fresh-account)))))
+
+(def statement[ "15/10" {:transactions ["Deposit 1000"] :balance "1000.00"}
+    "16/10" {:transactions ["Purchase on Amazon 3.34" "Purchase on Uber 45.23"] :balance "951.43"}
+    "17/10" {:transactions ["Withdrawal 180"] :balance "771.43"}
+    "18/10" {:transactions ["Purchase of a flight 800"] :balance "-28.57"}
+    "22/10" {:transactions ["Purchase of a expresso 10"] :balance "-38.57"}])
+
+(deftest Get-statement
+  (testing "Build array with statement info"
+      (is (= (get-statement @db/transactions (@db/accounts account-id) {:init "15/10" :end "23/10"}) statement))
+      (is (= (get-statement @db/transactions (@db/accounts account-id) {:init "16/10" :end "19/10"}) (subvec statement 2 8)))
+      (is (empty? (get-statement @db/transactions fresh-account {:init "15/10" :end "23/10"})))))
