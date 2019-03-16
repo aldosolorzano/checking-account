9073309966bf343027e1a938c95c5450a95d31c4
aldo <aldosolorzanop@gmail.com>
2019-03-15 18:16:02 -0600

    refactor balance & statement functions

--- src/checking_account/core.clj
+++ src/checking_account/core.clj
@@ -8,14 +8,15 @@
 (defn get-balance
   [transactions account]
   (let [tx-ids (deref (account :tx-ids))
-        balance (if (empty? tx-ids) 0.00 (b/compute-balance transactions tx-ids))]
+        ; balance (if (empty? tx-ids) 0.00 (b/compute-balance transactions tx-ids))]
+        balance (b/compute-balance (db/account-txs transactions account))]
     (b/format-float  balance)))
 
 (defn negative-periods
   [transactions account]
   (np/add-end-to-negative-txs
    (b/negative-balances (db/account-txs transactions account))
-   (b/compute-balance transactions (deref (account :tx-ids)))
+   (b/compute-balance (db/account-txs transactions account))
    (last (db/account-txs transactions account))))
 
 (defn get-statement
@@ -23,5 +24,5 @@
    (let [init (date-params :init)
          end  (date-params :end)]
      (if (and (d/valid-date? init) (d/valid-date? end))
-       (b/add-balance-to-statement transactions (st/build-statement transactions account) date-params)
+       (st/build-statement transactions account date-params)
        {:errors "Invalid date interval, try :init 11/10 :end 26/10"})))
