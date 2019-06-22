c9fde01fba8d32c3179ac11fa7ea994f4c50c7e4
aldo <aldosolorzanop@gmail.com>
2019-06-22 01:39:11 -0500

    Refactor balance functions

--- src/checking_account/balance.clj
+++ src/checking_account/balance.clj
@@ -8,33 +8,22 @@
 
 (defn compute-balance
   [transactions]
-  (loop [remaining transactions
-         balance 0]
-    (if (empty? remaining)
-      balance
-      (do
-        (let [tx (first remaining)
-          next-remaining (rest remaining)
-          current-balance (if (= (tx :type) :deposit)
-                            (+ balance (tx :amount))
-                            (- balance (tx :amount)))]
-        (recur next-remaining current-balance))))))
+  (apply + (map :amount transactions)))
+
+(defn build-period
+  [{:keys [amount date]} balance]
+  {:principal (+ balance amount) :start (d/unparse-date date) })
+
+(defn conj-results
+  [results tx]
+  (conj results (build-period tx (:principal (last results)))))
+
+(defn build-balances
+  [transactions]
+  (reduce #(conj-results % %2) [(build-period (first transactions) 0)] (rest transactions)))
 
 (defn negative-balances
   [transactions]
-  (loop [remaining transactions
-         results []
-         prev-balance 0]
-    (if (empty? remaining)
-      results
-      (do
-        (let [tx (first remaining)
-          date (d/unparse-date (tx :date))
-          next-remaining (rest remaining)
-          current-balance (if (= (tx :type) :deposit)
-                            (+ prev-balance (tx :amount))
-                            (- prev-balance (tx :amount)))
-          next-results (if (neg? current-balance)
-                         (conj results {:principal (format-float (* current-balance -1)) :start date})
-                         results)]
-        (recur next-remaining next-results current-balance))))))
+  (->> (build-balances transactions)
+       (filter #(neg? (:principal %)))
+       (map #(assoc % :principal  (format-float (- (:principal %))))))) ; convert balance to pos
