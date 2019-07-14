370071d2478512396ca3f0e08f6dabb83b00c834
aldo <aldosolorzanop@gmail.com>
2019-07-13 19:23:50 -0500

    Remove unnecessary functions, in balance.clj & add tests

--- src/checking_account/balance.clj
+++ src/checking_account/balance.clj
@@ -1,37 +1,18 @@
-(ns checking-account.balance
-  (:require
-   [checking-account.date-helpers :as d]
-   [checking-account.db :as db]))
+(ns checking-account.balance)
 
-(defn format-float
-  [number]
+(defn format-float [number]
   (format "%.2f"  (float number)))
 
-(defn compute-balance
-  [transactions]
+(defn compute-balance [transactions]
   (apply + (map :amount transactions)))
 
-(defn build-period
-  ([{:keys [amount date]}]
-   (hash-map :principal amount :start (d/unparse-date date)))
-  ([{:keys [amount date]} last-tx]
-   (hash-map :principal (+ (:principal last-tx) amount) :start (d/unparse-date date))))
-
-(defn conj-results
-  [builder results tx]
+(defn conj-results [builder results tx]
   (if (empty? results)
    (conj results (builder tx))
    (conj results (builder tx (last results)))))
 
 ; transactions -> vector/sequence of transactions.
-; builder -> function that uses tx's body to return data.
+; builder -> function that uses tx's body to return data. One or two args
 ; returns a vector of the data given by the builder function.
-(defn build-balances
-  [transactions builder]
+(defn build-balances [transactions builder]
   (reduce #(conj-results builder % %2) [] transactions))
-
-(defn negative-balances
-  [transactions]
-  (->> (build-balances transactions build-period)
-       (filter #(neg? (:principal %)))
-       (map #(assoc % :principal  (format-float (Math/abs (:principal %))))))) ; convert balance to pos
