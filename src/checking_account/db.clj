93d04bc90f0e56c9fee82780f2aff3cd6e289e72
aldo <aldosolorzanop@gmail.com>
2019-07-24 19:58:04 -0500

    Refactor db.clj, fix concurrency problems

--- src/checking_account/db.clj
+++ src/checking_account/db.clj
@@ -2,25 +2,52 @@
   (:require [checking-account.date-helpers :as d]
             [clojure.string :as string]))
 
-(def accounts (atom {100 {:id 100 :tx-ids (atom [1 2 3 4 5 6 7])}
-                     200 {:id 200 :tx-ids (atom [])}}))
+(def accounts (atom {100 {:id 100 :tx-ids (ref [1 2 3 4 5 6])}
+                     200 {:id 200 :tx-ids (ref [])}}))
 
-(def transactions (atom {1 {:id 1 :account 100 :description "Deposit"
-                           :amount 1000 :date (d/parse-date "11/10/2019") :type :deposit}
-                         2 {:id 2 :account 100 :description "Purchase on Amazon"
-                            :amount -3.34 :date (d/parse-date "16/10/2019") :type :purchase}
-                         3 {:id 3 :account 100 :description "Purchase on Uber"
-                            :amount -45.23 :date (d/parse-date "14/10/2019") :type :purchase}
-                         4 {:id 4 :account 100 :description "Withdrawal"
-                            :amount -180 :date (d/parse-date "17/10/2019") :type :withdrawal}
-                         5 {:id 5 :account 100 :description "Purchase of a flight"
-                            :amount -800 :date (d/parse-date "12/10/2019") :type :purchase}
-                         6 {:id 6 :account 100 :description "Purchase of a expresso"
-                            :amount -10 :date (d/parse-date "22/10/2019") :type :purchase}
-                         7 {:id 7 :account 100 :description "Deposit"
-                          :amount -500 :date (d/parse-date "22/10/2019") :type :purchase}}))
+(def transactions (ref {1 {:id 1
+                            :account 100
+                            :description "Deposit"
+                            :amount 1000
+                            :date (d/parse-date "11/10/2019")
+                            :type :deposit}
+                         2 {:id 2
+                            :account 100
+                            :description "Purchase on Amazon"
+                            :amount -3.34
+                            :date (d/parse-date "16/10/2019")
+                            :type :purchase}
+                         3 {:id 3
+                            :account 100
+                            :description "Purchase on Uber"
+                            :amount -45.23
+                            :date (d/parse-date "14/10/2019")
+                            :type :purchase}
+                         4 {:id 4
+                            :account 100
+                            :description "Withdrawal"
+                            :amount -180
+                            :date (d/parse-date "17/10/2019")
+                            :type :withdrawal}
+                         5 {:id 5
+                            :account 100
+                            :description "Purchase of a flight"
+                            :amount -800
+                            :date (d/parse-date "12/10/2019")
+                            :type :purchase}
+                         6 {:id 6
+                            :account 100
+                            :description "Purchase of a expresso"
+                            :amount -10
+                            :date (d/parse-date "22/10/2019")
+                            :type :purchase}}))
 
-(def tx-id (atom (count @transactions)))
+(def tx-validators  {:description string?
+                     :date d/valid-date?
+                     :amount number?
+                     :type string?})
+
+(def tx-id (atom 6))
 
 (def account-id (atom 100))
 
@@ -29,11 +56,11 @@
 
 (defn create-account []
   (let [id (swap! account-id inc)
-        accs (swap! accounts conj (build-account id))
         res {:id id}]
+    (swap! accounts conj (build-account id)
     (print "Account created: ")
     (println res)
-    res))
+    res)))
 
 (defn build-tx [params]
   (let [{:keys [date amount type description]} params]
@@ -42,29 +69,21 @@
     :description description
     :type (keyword type)}))
 
-(defn errors-in-tx-params [params]
-  (loop
-    [remaining [:description :date :amount :type]
-     errors []]
-    (if (empty? remaining) errors
-      (let [param (first remaining)
-            err (case param
-                  :description (if-not (string? (params param)) param false)
-                  :date (if-not (d/valid-date? (params param)) param false)
-                  :amount (if-not (number? (params param)) param false)
-                  :type (if-not (string? (params param)) param false))
-            next-errors (if (false? err) errors (conj errors err))]
-        (recur (rest remaining) next-errors)))))
+(defn errors-in-tx-params [tx-params]
+  (for [[param type] (seq tx-validators)
+        :when (false? (type (param tx-params)))]
+        param))
 
 (defn save-transaction [account tx-params]
   (dosync
     (let [id (swap! tx-id inc)
-          tx (assoc (build-tx tx-params) :id id)]
-     (swap! transactions conj {id tx})
-     (swap! (:tx-ids account) conj id))))
+          tx (assoc (build-tx tx-params) :id id :account (:id account))]
+     ; Use of alter to apply STM dependency rules
+     (alter transactions conj {id tx})
+     (alter (:tx-ids account) conj id))))
 
 (defn create-transaction [placeholder account params]
-    ; TODO remove placeholder, it is pased in handler.clj account-finder to valide account
+  ; TODO remove placeholder, it is pased in handler.clj account-finder to valide account
   (let [param-errors (errors-in-tx-params params)]
     (if (empty? param-errors)
      (update (save-transaction account params) :date d/unparse-date)
--- test/checking_account/db_test.clj
+++ test/checking_account/db_test.clj
@@ -1,34 +1,48 @@
 (ns checking-account.db-test
-  (:import java.util.concurrent.Executors)
   (:require
    [clojure.test :refer :all]
    [checking-account.db :refer :all]
    [checking-account.date-helpers :as d]))
 
-(def thread-pool
-  (Executors/newFixedThreadPool
-  (+ 2 (.availableProcessors (Runtime/getRuntime)))))
-
-(defn dothreads!
-  [f & {thread-count :threads
-        exec-count   :times
-        :or {thread-count 1 exec-count 1}}]
-  (dotimes [t thread-count]
-    (.submit thread-pool
-             #(dotimes [_ exec-count] (f)))))
-
-(def body-params {:amount 100 :date "28/08/2019" :type "deposit" :description "blaj"})
-
 (def local-accounts [(get @accounts 100) (get @accounts 200)])
 
-(defn call-create-tx []
-  (create-transaction "_"  (rand-nth local-accounts) body-params))
+(def tx {:description "Large purchase"
+         :amount 500
+         :date "22/10/2019"
+         :type "purchase"})
 
-(deftest Concurrency-test
-  (testing "Correct id generation"
-    (dothreads! call-create-tx :threads 100 :times 100)
-    (is (= (count @transactions) (last (sort (flatten (map (comp deref :tx-ids) local-accounts))))))
-    (is (= @tx-id (count @transactions)))))
+(deftest Errors-in-tx-params
+  (testing "validate each tx param applying validator fn, defined in file"
+    (is (= (errors-in-tx-params {:amount 15}) '(:description :date :type)))
+    (is (empty? (errors-in-tx-params tx)))))
+
+(def valid-tx {:date (d/parse-date "22/10/2019")
+               :amount -500
+               :description "Large purchase"
+               :type :purchase})
+
+(deftest Build-tx
+  (testing "building correct tx body"
+    (is (= (build-tx tx) valid-tx))))
+
+
+(deftest Save-transaction
+  (testing "saving transaction in atom"
+    (is (= (save-transaction {:tx-ids (ref [1 2])} tx) (conj [1 2] @tx-id)))))
+
+(def account {:tx-ids (ref [1 2])})
+(def txs {1 {:date (d/parse-date "11/10/2019")}
+          2 {:date (d/parse-date "08/05/2019")}
+          3 {:date (d/parse-date "25/06/2019")}})
 
 (deftest Account-txs
-  (println (account-txs @transactions (first local-accounts))))
+  (testing "return account transactions sorted"
+    (is (empty? (account-txs txs {:tx-ids (ref [])})))
+    (is (= (account-txs txs account) [{:date (d/parse-date "08/05/2019")}
+                                      {:date (d/parse-date "11/10/2019")}]))))
+
+(deftest Get-account-by-id
+  (testing "fetching account from accounts hash-map"
+    (is (= (get-account-by-id @accounts 100) (get @accounts 100)))
+    (is (= (get-account-by-id @accounts "100") (get @accounts 100)))
+    (is (nil? (get-account-by-id @accounts "500")))))
